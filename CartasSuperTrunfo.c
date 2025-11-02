0d8f5718dd114a8f727613ee615a0ca708f1b1ec
mobloide <139916026+mobloide@users.noreply.github.com>
2025-11-02 15:18:16 +0000

    fim

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -80,20 +80,22 @@ int main() {
 
     // exibição da primeria carta;
 
-    printf("\ncarta1\n");
+    printf("\nCarta 1:\n");
     printf("Estado: %c\n", estado1);
     printf("Código: %s\n", codigo1);
     printf("Nome da Cidade: %s\n", cidade1);
+    printf("População: %d\n", populacao1);
     printf("Área: %.2f km²\n", area1);
     printf("PIB: %2.F bilhões de reais\n", pib1);
     printf("Número de Pontos Turísticos: %d\n", pontos_turisticos1);
 
     // exibição da segunda carta; 
 
-    printf("\ncarta2\n");
+    printf("\nCarta 2:\n");
     printf("Estado: %c\n", estado2);
     printf("Código: %s\n", codigo2);
     printf("Nome da Cidade: %s\n", cidade2);
+    printf("População: %d\n", populacao2);
     printf("Área: %.2f km²\n", area2);
     printf("PIB: %2.F bilhões de reais\n", pib2);
     printf("Número de Pontos Turísticos: %d\n", pontos_turisticos2);
