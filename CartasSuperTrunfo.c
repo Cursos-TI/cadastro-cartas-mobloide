f02a76dacbb73749cfe9ff8c65f1a9b6026d6245
mobloide <139916026+mobloide@users.noreply.github.com>
2025-11-02 14:40:44 +0000

    Esta pronto

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -19,7 +19,7 @@ int main() {
     // Declaração das variaveis da segunda carta
 
     char estado2;
-    char codigo2[1];
+    char codigo2[5];
     char cidade2[30];
     int populacao2;
     float area2;
@@ -36,7 +36,7 @@ int main() {
     scanf("%s", codigo1);
 
     printf("Digite o Nome da Cidade: ");
-    scanf("%[\n]", cidade1); //leia até o enter
+    scanf("%s", cidade1); //leia até o enter
 
     printf("Digite a Populacao: ");
     scanf("%d", &populacao1);
@@ -61,7 +61,7 @@ int main() {
     scanf("%s", codigo2);
 
     printf("Digite o Nome da Cidade: ");
-    scanf("%[\n]", cidade2); //leia até o enter
+    scanf("%s", cidade2); //leia até o enter
 
     printf("Digite a Populacao: ");
     scanf("%d", &populacao2);
@@ -79,25 +79,21 @@ int main() {
 
     printf("\ncarta1\n");
     printf("Estado: %c\n", estado1);
-    printf("Codigo: %s\n", codigo1);
+    printf("Código: %s\n", codigo1);
     printf("Nome da Cidade: %s\n", cidade1);
-    printf("Area: %.2f km²\n", area1);
-    printf("PIB: %2.F Bilhoes de reais\n", pib1);
-    printf("Numero de Pontos Turisticos: %d\n", pontos_turisticos1);
+    printf("Área: %.2f km²\n", area1);
+    printf("PIB: %2.F bilhões de reais\n", pib1);
+    printf("Número de Pontos Turísticos: %d\n", pontos_turisticos1);
     
 
     printf("\ncarta2\n");
     printf("Estado: %c\n", estado2);
-    printf("Codigo: %s\n", codigo2);
+    printf("Código: %s\n", codigo2);
     printf("Nome da Cidade: %s\n", cidade2);
-    printf("Area: %.2f km²\n", area2);
-    printf("PIB: %2.F Bilhoes de reais\n", pib2);
-    printf("Numero de Pontos Turisticos: %d\n", pontos_turisticos2);
+    printf("Área: %.2f km²\n", area2);
+    printf("PIB: %2.F bilhões de reais\n", pib2);
+    printf("Número de Pontos Turísticos: %d\n", pontos_turisticos2);
 
-
-// brasilia esta colocada de maneira errada, lembrar de corrigir mais tarde;
-
-// como vou fazer para que a depuracao nao necssite de interassao posterior?
 
   // Área para definição das variáveis para armazenar as propriedades das cidades
 
